835f653d112a554d3e37cfcd1d2afb2749355745
Daniel-MB <Daniel-MB@Daniel-MBui-MacBook-Pro.local>
2017-05-13 15:54:09 +0900

    add redux & reducer

--- session11/react-example/src/component/App.js
+++ session11/react-example/src/component/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../routes/Home';
 import Posts from '../routes/Posts';
 import Menu from './Menu';
+import NotExist from './NotExist';
 
 class App extends Component {
     render() {
@@ -10,8 +11,11 @@ class App extends Component {
             <Router>
                 <div>
                     <Menu />
-                    <Route exact path="/" component={Home} />
-                    <Route path="/posts" component={Posts} />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/posts" component={Posts} />
+                        <Route component={NotExist} />
+                    </Switch>
                 </div>
                 
             </Router>
--- session11/react-example/src/routes/Posts.js
+++ session11/react-example/src/routes/Posts.js
@@ -1,18 +1,55 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import PostList from '../component/PostList';
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
+import { Route, Link } from 'react-router-dom'; // 서브 라우터 사용하기위해.
+import PostModal from '../component/PostModal';
 
-const Posts = (props) => {
-    return (
-        <div>
-            포스트
-            <Link to="/posts/1">1번째 포스트</Link>
-            <Link to="/posts/2">2번째 포스트</Link>
-            <Link to="/posts/3">3번째 포스트</Link>
+class Posts extends Component {
 
-            <Route path="/posts/:postId" component={PostList} />
-        </div>    
-    );
+    constructor() {
+        super();
+
+        this.state = {
+            posts: [
+                { title: "첫번째글", created_at: "2017-05-13" },
+                { title: "두번째글", created_at: "2017-04-20" },
+                { title: "세번째글", created_at: "2017-03-10" },
+                { title: "네번째글", created_at: "2017-02-05" },
+                { title: "다섯번째글", created_at: "2017-01-22" },
+            ]
+        }
+    }
+
+    render() {
+        return (
+
+            <div>
+                <Table bordered hover>
+                    <thead>
+                        <tr>
+                            <th>제목</th>
+                            <th>날짜</th>
+                            <th>삭제</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.posts.map( (post, key) => {
+                                return (
+                                    <tr key={ key }>
+                                        <td><Link to={`/posts/${key}`}>{ post.title }</Link></td>
+                                        <td>{ post.created_at }</td>
+                                        <td></td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </Table>
+
+                <Route path="/posts/:id" component={PostModal} />
+            </div>
+        );
+    }
 }
 
 export default Posts;
